d01e918d0af3e503562df218ab85ce2f8bbf7d5e
kkanchamreddy <kiran.kanchamreddy@gmail.com>
2016-01-10 09:47:12 -0800

    Add more UI widgets to layout

--- app/src/main/java/com/kkiran/criminalintent/fragment/CrimeFragment.java
+++ app/src/main/java/com/kkiran/criminalintent/fragment/CrimeFragment.java
@@ -8,6 +8,9 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 import com.kkiran.criminalintent.R;
@@ -19,6 +22,8 @@ import com.kkiran.criminalintent.model.Crime;
 public class CrimeFragment extends Fragment {
     private Crime mCrime;
     private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckbox;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,6 +35,7 @@ public class CrimeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
         mTitleField = (EditText)v.findViewById(R.id.crime_title);
         mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
@@ -47,6 +53,20 @@ public class CrimeFragment extends Fragment {
 
             }
         });
+
+        mDateButton = (Button)v.findViewById(R.id.crime_date);
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckbox = (CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
+
+
         return v;
     }
 }
--- app/src/main/java/com/kkiran/criminalintent/model/Crime.java
+++ app/src/main/java/com/kkiran/criminalintent/model/Crime.java
@@ -1,5 +1,6 @@
 package com.kkiran.criminalintent.model;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -8,6 +9,29 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime(){
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
 
     public UUID getId() {
         return mId;
