3cf806220d2d0cf527797d057fd5fcfeecdd22b7
kkanchamreddy <kiran.kanchamreddy@gmail.com>
2016-01-11 08:06:37 -0800

    Setup Recyclerview

--- app/src/main/java/com/kkiran/criminalintent/fragment/CrimeListFragment.java
+++ app/src/main/java/com/kkiran/criminalintent/fragment/CrimeListFragment.java
@@ -1,9 +1,30 @@
 package com.kkiran.criminalintent.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kkiran.criminalintent.R;
 
 /**
  * Created by kkanchamreddy on 1/10/16.
  */
 public class CrimeListFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime_list, container, false);
+
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.crime_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        
+        return v;
+    }
 }
