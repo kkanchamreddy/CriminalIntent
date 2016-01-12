8a91644cf865a219540b89ef32e900ebd2449054
kkanchamreddy <kiran.kanchamreddy@gmail.com>
2016-01-12 08:16:35 -0800

    Add ViewHolder and Adapter

--- app/src/main/java/com/kkiran/criminalintent/fragment/CrimeListFragment.java
+++ app/src/main/java/com/kkiran/criminalintent/fragment/CrimeListFragment.java
@@ -8,8 +8,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.kkiran.criminalintent.R;
+import com.kkiran.criminalintent.model.Crime;
+
+import java.util.List;
 
 /**
  * Created by kkanchamreddy on 1/10/16.
@@ -24,7 +28,44 @@ public class CrimeListFragment extends Fragment {
 
         mRecyclerView = (RecyclerView) v.findViewById(R.id.crime_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        
+
         return v;
+    }
+
+    private class CrimeViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView mTitleTextView;
+
+        public CrimeViewHolder(View itemView) {
+            super(itemView);
+            mTitleTextView = (TextView) itemView;
+        }
+    }
+
+    private class CrimeAdapter extends  RecyclerView.Adapter<CrimeViewHolder> {
+        private List<Crime> mCrimes;
+
+        public CrimeAdapter(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
+
+        @Override
+        public CrimeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater
+                    .inflate(android.R.layout.simple_list_item_1, parent, false);
+            return new CrimeViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(CrimeViewHolder holder, int position) {
+            Crime crime = mCrimes.get(position);
+            holder.mTitleTextView.setText(crime.getTitle());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCrimes.size();
+        }
     }
 }
