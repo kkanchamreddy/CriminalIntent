cd682c37c0522e4f9630995f87e5e7ef911c0b8c
kkanchamreddy <kiran.kanchamreddy@gmail.com>
2016-01-12 22:53:55 -0800

    Tie up ViewHolder and Adapter

--- app/src/main/java/com/kkiran/criminalintent/fragment/CrimeListFragment.java
+++ app/src/main/java/com/kkiran/criminalintent/fragment/CrimeListFragment.java
@@ -8,10 +8,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import com.kkiran.criminalintent.R;
 import com.kkiran.criminalintent.model.Crime;
+import com.kkiran.criminalintent.model.CrimeLab;
 
 import java.util.List;
 
@@ -21,6 +23,8 @@ import java.util.List;
 public class CrimeListFragment extends Fragment {
 
     private RecyclerView mRecyclerView;
+    private CrimeAdapter mCrimeAdapter;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -29,16 +33,38 @@ public class CrimeListFragment extends Fragment {
         mRecyclerView = (RecyclerView) v.findViewById(R.id.crime_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        updateUI();
         return v;
+    }
+
+    private void updateUI() {
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        List<Crime> crimes = crimeLab.getCrimes();
+
+        mCrimeAdapter = new CrimeAdapter(crimes);
+        mRecyclerView.setAdapter(mCrimeAdapter);
     }
 
     private class CrimeViewHolder extends RecyclerView.ViewHolder {
 
-        public TextView mTitleTextView;
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+        private CheckBox mSolvedCheckbox;
+
+        private Crime mCrime;
 
         public CrimeViewHolder(View itemView) {
             super(itemView);
-            mTitleTextView = (TextView) itemView;
+            mTitleTextView = (TextView) itemView.findViewById(R.id.list_item_crime_title_text_view);
+            mDateTextView = (TextView) itemView.findViewById(R.id.list_item_crime_date_text_view);
+            mSolvedCheckbox= (CheckBox) itemView.findViewById(R.id.list_item_crime_solved_checkbox);
+        }
+
+        public void bindCrime(Crime crime) {
+            mCrime = crime;
+            mTitleTextView.setText(mCrime.getTitle());
+            mDateTextView.setText(mCrime.getDate().toString());
+            mSolvedCheckbox.setChecked(mCrime.isSolved());
         }
     }
 
@@ -53,14 +79,15 @@ public class CrimeListFragment extends Fragment {
         public CrimeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
             View view = layoutInflater
-                    .inflate(android.R.layout.simple_list_item_1, parent, false);
+                    .inflate(R.layout.list_item_crime, parent, false);
             return new CrimeViewHolder(view);
         }
 
         @Override
         public void onBindViewHolder(CrimeViewHolder holder, int position) {
             Crime crime = mCrimes.get(position);
-            holder.mTitleTextView.setText(crime.getTitle());
+            //holder.mTitleTextView.setText(crime.getTitle());
+            holder.bindCrime(crime);
         }
 
         @Override
